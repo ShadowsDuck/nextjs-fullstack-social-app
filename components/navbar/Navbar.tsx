f2dc73f8a7c70a81083cdf6e82be85ea159790f3
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-04-07 04:48:37 +0700

    - Set up Project. - Created DesktopNavbar and MobileNavbar. - Added DarkMode. - Implemented ModeToggle component for theme switching. - Configured middleware for Clerk authentication.

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import DesktopNavbar from './DesktopNavbar'
+import MobileNavbar from './MobileNavbar'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <Link href="/" className="text-primary font-mono text-xl font-bold tracking-wider">
+              Socially
+            </Link>
+          </div>
+
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
+    </nav>
+  )
+}
+export default Navbar
